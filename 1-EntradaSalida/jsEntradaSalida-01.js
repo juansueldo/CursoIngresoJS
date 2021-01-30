cb0d1d5e96cfe7c9266b44778a413eacbdf881f4
juansueldo <juan.sueldo1989@gmail.com>
2021-01-30 11:58:57 -0300

    Tercer Commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,5 +3,5 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo 2021");
+	alert("Esto funciona de maravilla");
 }
