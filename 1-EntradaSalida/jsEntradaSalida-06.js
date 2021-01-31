0d1c79097635fd28f4968b8c02e09dcf1b2354bc
juansueldo <juan.sueldo1989@gmail.com>
2021-01-31 14:10:14 -0300

    Commit 2 Domingo

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,18 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numero1;
+	let numero2;
+
+
+	numero1 = document.getElementsByClassName("txtIdNumeroUno").value;
+	numero2 = document.getElementsByName("txtIdNumeroDos").value;
+
+	let resultado;
+	resultado = numero1 + numero2;
+
+	
+	alert("La suma es " + resultado);
+
 }
 
