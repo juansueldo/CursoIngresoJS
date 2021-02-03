bfce4517c997213a5ac040f20ab051f10c81831f
juansueldo <juan.sueldo1989@gmail.com>
2021-02-03 14:18:04 -0300

    adelantado if

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -5,8 +5,16 @@ function mostrar()
 
 	edad = parseInt(document.getElementById("txtIdEdad").value);
 
-	if(edad==15)
+	//Luego de if colocamos entre parentesis la codicion, luego de cerrar parentesis
+	//abrimos un corchete para ingresar lo que nos debe imprimir en pantalla
+	//finalizamos cerrando los correchetes
+	if(edad==15){
 	
 	alert("La niña bonita");
+	}
 
-}//FIN DE LA FUNCIÓN
+	//Podemos imprimir en pantalla luego del if el final de la función
+	alert("Fin de la función");
+
+//FIN DE LA FUNCIÓN
+}
