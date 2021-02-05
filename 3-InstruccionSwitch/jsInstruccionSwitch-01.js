a603e8d89484f79b504e52eaac5b01c3b76c1aaa
juansueldo <juan.sueldo1989@gmail.com>
2021-02-05 12:45:37 -0300

    TP4IF

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,32 @@
 function mostrar()
 {
+	let mesDelAño;
+	let enero = 1;
+	let marzo = 2;
+	let julio = 3;
+	let diciembre = 4;
+
+	
+
+	mesDelAño= document.getElementById("txtIdMes").value;
+	switch(mesDelAño){
+		case 1:
+			alert("que comiences bien el año!!!.");
+			break;
+		case 2:
+			 alert("a clases!!!.");
+		case 3:
+			alert("se vienen las vacaciones!!!.");
+		case 4:
+			alert("Felices fiesta!!!.");
+
+	}
+
 	//tomo el mes
+	/*
 	var mesDelAño =txtIdMes.value;
 	alert(mesDelAño);
-
+*/
 
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,12 +10,63 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
-     let cantidad_lamparas;
-     const PRECIO_LAMAPARA;
-     
-     PRECIO_LAMAPARA=35;
-     cantidad_lamparas=parseInt(document.getElementById("txtIdCantidad").value);
+    const PRECIO = 35;
+    let cantidad;
+    let marca;
+    let descuento;
+    let importeFinal;
+    let iibb;
+    let precioConDescuento;
 
-     if(cantidad_lamparas>=6)
-     alert(PRECIO_LAMAPARA*cantidad_lamparas/2);
+    cantidad = parseInt(document.getElementById("txtIdCantidad").value);
+    marca = document.getElementById("Marca").value;
+
+    switch(cantidad){
+         case 1:
+         case 2:
+               descuento =0;
+               break;
+          case 3:
+               if(marca == "ArgentinaLuz"){
+                    descuento = 15;
+               }
+               else if(marca == "FelipeLamparas"){
+                    descuento = 10;
+               }
+               else{
+                    descuento = 5;
+               }
+               break;
+          case 4:
+               if(marca == "ArgentinaLuz" || marca == "FelipeLamparas"){
+                    descuento = 25;
+               }
+               else{
+                    descuento =20;
+               }
+               break;
+          case 5:
+               if(marca == "ArgentinaLuz"){
+               descuento = 40;
+               }
+               else{
+                    descuento =30;
+               }
+               break;
+          default:
+               descuento = 50;
+    }
+    precioConDescuento = PRECIO - PRECIO * descuento/100;
+    document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+    importeFinal = precioConDescuento * cantidad;
+
+    if(importeFinal>=120){
+         iibb = importeFinal * 10/100;
+         importeFinal = importeFinal + iibb;
+
+         alert("Importe a pagar $"+ importeFinal + ". Usted pagó $"+iibb+" de Ingresos Brutos.");
+    }
+    else{
+         alert("Importe total $"+importeFinal);
+    }
 }
