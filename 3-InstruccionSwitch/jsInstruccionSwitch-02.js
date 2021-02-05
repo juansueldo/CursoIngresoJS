ad43e4ea5da11b893946b34cedd9b093b41d8967
juansueldo <juan.sueldo1989@gmail.com>
2021-02-05 15:45:04 -0300

    Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,18 +1,51 @@
-function mostrar()
-{
+function mostrar() {
 	//tomo el mes
 	let mes;
 
 	mes = document.getElementById("txtIdMes").value;
-	switch(mes){
+	switch (mes) {
+		case "Julio" || "Agosto":
+		alert("Abrigate que hace frio");
+			break;
+	}
+		/*
 		case "Julio":
 			alert("Abrigate que hace frio");
-	    case "Agosto":
+			break;
+		case "Agosto":
 			alert("Abrigate que hace frio");
+			break;
+		case "Septiembre":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		case "Octubre":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		case "Noviembre":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		case "Diciembre":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		case "Enero":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		case "Febrero":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		case "Marzo":
+			alert("Falta para el invierno");
+			break;
+		case "Abril":
+			alert("Falta para el invierno");
+			break;
+		case "Mayo":
+			alert("Falta para el invierno");
+			break;
+		case "Junio":
+			alert("Falta para el invierno");
+			break;
 	}
-	alert(mesDelAño);
-
-
-
+*/
 
 }//FIN DE LA FUNCIÓN
