ccde2d13fbad586cfd9dd65a02260af72e539d23
juansueldo <juan.sueldo1989@gmail.com>
2021-02-10 15:58:37 -0300

    terminar ejercicio 10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,31 @@
+/*una agencia de viajes nos piden informar si hacemos viajes a lugares según la estación del año estemos, 
+informar si "Se viaja" o "No se viaja" a ese lugar
+
+en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
+
+en Verano: Se viaja a Mar del plata y Cataratas solamente
+
+en Otoño: Se viaja a todos los destinos.
+
+primavera: solo no se viaja a Bariloche*/
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
+	var estacionIngresada;
+	var localidad;
+
+	estacionIngresada = document.getElementById("txtIdEstacion").value;
+	localidad = document.getElementById("txtIdDestino").value;
+
+	switch(estacionIngresada){
+		case "Invierno":
+			if(localidad == "Bariloche"){
+			alert("Se viaja");
+		}
+		else{
+			alert("No se viaja");s
+		}
+	}
+
 	alert(estacionIngresada);
 
 }//FIN DE LA FUNCIÓN
